b353bcf9e3ab54d73dee99b81003f41aa93516a4
Jason Lengstorf <jason@lengstorf.com>
2019-02-18 21:18:05 -0600

    fix: add page titles to post preview pages (#2)

---
+++ src/hooks/use-site-metadata.js
@@ -0,0 +1,36 @@
+import { useStaticQuery, graphql } from 'gatsby';
+
+const useSiteMetadata = () => {
+  const result = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+          canonicalUrl
+          image
+          author {
+            name
+          }
+          organization {
+            name
+            url
+            logo
+          }
+          social {
+            twitter
+            fbAppID
+          }
+          categories {
+            name
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  return result.site.siteMetadata;
+};
+
+export default useSiteMetadata;
