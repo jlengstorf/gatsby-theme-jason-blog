4864217ba7baf59c6c243616f1afa2e259f547ca
Jason Lengstorf <jason@gatsbyjs.com>
2019-06-04 15:24:44 -0700

    fix: upgrade dependencies (#6)

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,11 +97,5 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: 'gatsby-plugin-compile-es6-packages',
-      options: {
-        modules: ['gatsby-theme-jason-blog'],
-      },
-    },
   ],
 };
